f14a7f132a6711b1fa7f6bb84519b8b75b93d710
envyandgreed <wangsihan_envy@foxmail.com>
2017-05-22 19:51:13 +0800

    addfiles

---
+++ newFourtyOne.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sihanwang on 2017/5/21.
+ */
+public class newFourtyOne {
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+            
+    }
+}
