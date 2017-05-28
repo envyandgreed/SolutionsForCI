952ee32152accf3f26444dc714b6d1b76307450b
envyandgreed <wangsihan_envy@foxmail.com>
2017-05-28 23:13:15 +0800

    addfiles

--- newFiftyFour.java
+++ newFiftyFour.java
@@ -4,4 +4,67 @@ package chapter8;
  * Created by sihanwang on 2017/5/25.
  */
 public class newFiftyFour {
+    public boolean isNumeric(char[] str){
+        int length = str.length;
+        int index = 0;
+
+        if (length == 0 || str == null){
+            return false;
+        }
+
+        while(index < length && str[index] == ' '){
+            index++;
+        }
+        while(str[length - 1] == ' '){
+            index--;
+        }
+
+
+        if (str[index] == '-' || str[index] == '+'){
+            index++;
+        }
+        if (index == length){
+            return false;
+        }
+
+
+        if (index < length && str[index] >= '0' && str[index] <= '9'){
+                index++;
+        }
+
+        if (index == length){
+            return  true;
+        }
+
+        if (str[index] == '.'){
+            index++;
+            if (index == length){
+                return true;
+            }
+            while(index < length && str[index] >= '0'&& str[index] <= '9'){
+                index++;
+            }
+            if (index == length){
+                return  true;
+            }
+        }
+
+        if (str[index] == 'e' || str[index] == 'E'){
+            index++;
+            if (index == length){
+                return false;
+            }
+            if (str[index] == '-' || str[index] == '+'){
+                index++;
+            }
+            while(index < length && str[index] >= '0'&& str[index] <= '9'){
+                index++;
+            }
+            if (index == length){
+                return  true;
+            }
+        }
+        return false;
+    }
+
 }
